e3c8e6b8902d471055a2510d36df4424f962f34e
taetaeyo <57551024+taetaeyo@users.noreply.github.com>
2023-12-05 15:02:47 +0900

    TestController commit

---
+++ src/main/java/com/okky/restserver/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.okky.restserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public String test() {
+		String data = "okky project return test";
+		System.out.println("TestController test");
+		
+		return data;
+	}
+}
