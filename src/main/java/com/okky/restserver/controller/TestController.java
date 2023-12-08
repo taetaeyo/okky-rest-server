965b11018c5065ab3085c35e41c94427e433e839
taetaeyo <57551024+taetaeyo@users.noreply.github.com>
2023-12-08 09:19:57 +0900

    slf4j Logger

--- src/main/java/com/okky/restserver/controller/TestController.java
+++ src/main/java/com/okky/restserver/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.okky.restserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 @RestController
 public class TestController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+	
 	@GetMapping("/test")
 	@Operation(summary = "Get test string value", description = "서버 접근 테스트를 위해 String을 반환한다.")
 	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "성공"),
 							@ApiResponse(responseCode = "404", description = "실패") })
 	public String test() {
 		String data = "okky project return test";
-		System.out.println("TestController test");
+		
+		LOGGER.info("Test Controller data :: " + data);
 		
 		return data;
 	}
