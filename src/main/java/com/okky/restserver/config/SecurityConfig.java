73588238bf30cec5250c0329f2c6fb6f8c361870
taetaeyo <57551024+taetaeyo@users.noreply.github.com>
2023-12-12 18:03:14 +0900

    Spring Security 임시 commit

--- src/main/java/com/okky/restserver/config/SecurityConfig.java
+++ src/main/java/com/okky/restserver/config/SecurityConfig.java
@@ -5,10 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.okky.restserver.service.UserDetailService;
 
@@ -16,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig{
 	
 	private final UserDetailsService userService;
@@ -28,24 +37,65 @@ public class SecurityConfig{
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests()
-                    .requestMatchers("/login", "/signup", "/user").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/articles")
-                .and()
-                .logout()
-                    .logoutSuccessUrl("/login")
-                    .invalidateHttpSession(true)
-                .and()
-                .csrf().disable()
-                .build();
+    	http
+    		.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests		//페이지 권한 설정
+	        .requestMatchers(
+	        			"/",
+		        		"/error",
+	                    "/api/**",
+	                    "/static/**",
+	                    "/auth/**",
+	                    "/test/**"
+    		).permitAll()	 				//securityFilter 제외 url
+	        .anyRequest().authenticated() 	//permitAll외 모든 경로는 인증이 필요
+    		)
+    		//로그인 설정
+            .formLogin(login -> login
+                    .loginPage("/auth/loginForm")
+                    .failureUrl("/auth/preout")
+                    .permitAll())
+          //로그아웃 설정
+            .logout(logout -> logout
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
+                    .logoutSuccessHandler(logoutSuccessHandler())
+            		.addLogoutHandler(new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(
+            				ClearSiteDataHeaderWriter.Directive.CACHE,
+            	            ClearSiteDataHeaderWriter.Directive.COOKIES,
+            	            ClearSiteDataHeaderWriter.Directive.STORAGE)))
+            		.invalidateHttpSession(true)
+                    .clearAuthentication(true))
+            //기본 예외설정
+//            .exceptionHandling(handling -> handling
+//                    .accessDeniedHandler(accessDeniedHandler()) //권한 접근 예외(Role)
+//                    .authenticationEntryPoint(enrtyPointHandler()))
+            .headers(headers -> headers
+            		.frameOptions((frameOptions) -> frameOptions
+            				.sameOrigin()
+            				.contentSecurityPolicy((contentSecurityPolicy) -> contentSecurityPolicy
+            						//.policyDirectives("default-src 'self'") //if required the full CSP(Contents Security Policy)
+            						.policyDirectives("form-action 'self';")
+            				)
+            			)
+            		)
+            .securityContext((securityContext) -> securityContext
+            		.securityContextRepository(
+            				new DelegatingSecurityContextRepository(
+            						new RequestAttributeSecurityContextRepository(),
+            						new HttpSessionSecurityContextRepository())
+            		)
+            		.securityContextRepository(new HttpSessionSecurityContextRepository()) // Spring security 6
+            ) 
+            .sessionManagement(management -> management
+                    .maximumSessions(1)
+                    .expiredUrl("/auth/preout")
+                    .sessionRegistry(sessionRegistry()));
+    		
+		http.csrf().disable();
+		
+		return http.build();
     }
 
-    @Bean
+	@Bean
     public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
                 .userDetailsService(userService)
@@ -57,5 +107,10 @@ public class SecurityConfig{
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    SpringSessionBackedSessionRegistry<? extends Session> sessionRegistry() {
+        return new SpringSessionBackedSessionRegistry<>(this.sessionRepository);
     }
 }
