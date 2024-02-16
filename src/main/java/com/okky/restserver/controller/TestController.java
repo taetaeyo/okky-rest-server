ceacf78c9b4467c86b89c11aee9d70ec965a6a21
taetaeyo <57551024+taetaeyo@users.noreply.github.com>
2024-02-16 17:34:28 +0900

    jenkins test

--- src/main/java/com/okky/restserver/controller/TestController.java
+++ src/main/java/com/okky/restserver/controller/TestController.java
@@ -34,7 +34,7 @@ public class TestController {
 				.status(ResponseCode.S0000.getStatus().value())
 				.code(ResponseCode.S0000.name())
 				.message(ResponseCode.S0000.getMessage())
-				.result("jenkins test11").build();
+				.result("jenkins test123123").build();
 		
 		log.info("Test Controller data {} ", data);
 		
